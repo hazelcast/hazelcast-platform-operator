e9fa451e913b2de7d1d4fbc9e0f881e25a5e0ee0
Rafal Leszko <rafal@hazelcast.com>
2021-06-29 16:26:19 +0200

    Revert - Verify if PR builder works correctly

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -60,5 +60,5 @@ var _ = BeforeSuite(func() {
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
 	err := testEnv.Stop()
-	Expect(err).To(HaveOccurred())
+	Expect(err).NotTo(HaveOccurred())
 })
