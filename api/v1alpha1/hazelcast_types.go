2fdd7ab88cbb4ce8c965c7468a2bdf098820aa8b
Rafal Leszko <rafal@hazelcast.com>
2021-07-27 15:10:09 +0200

    Update Hazelcast CR with expose externally configuration

--- api/v1alpha1/hazelcast_types.go
+++ api/v1alpha1/hazelcast_types.go
@@ -1,6 +1,7 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -11,18 +12,45 @@ type HazelcastSpec struct {
 	// +kubebuilder:default:=3
 	// +optional
 	ClusterSize int32 `json:"clusterSize"`
+
 	// Repository to pull the Hazelcast Platform image from.
 	// +kubebuilder:default:="docker.io/hazelcast/hazelcast-enterprise"
 	// +optional
 	Repository string `json:"repository"`
+
 	// Version of Hazelcast Platform.
 	// +kubebuilder:default:="5.0-SNAPSHOT"
 	// +optional
 	Version string `json:"version"`
-	// Name of the secret with Hazelcast Enterprise License Key
+
+	// Name of the secret with Hazelcast Enterprise License Key.
 	// +kubebuilder:default:="hazelcast-license-key"
 	// +optional
 	LicenseKeySecret string `json:"licenseKeySecret"`
+
+	// Configuration to expose Hazelcast cluster to external clients.
+	// +optional
+	ExposeExternally ExposeExternallyConfiguration `json:"exposeExternally"`
+}
+
+// ExposeExternallyConfiguration defines how to expose Hazelcast cluster to external clients
+type ExposeExternallyConfiguration struct {
+	// Method of how members are exposed.
+	// +kubebuilder:default:=Smart
+	// +kubebuilder:validation:Enum=Unisocket;Smart
+	// +optional
+	Type string `json:"type"`
+
+	// Type of the service used to discover Hazelcast cluster.
+	// +kubebuilder:default:=LoadBalancer
+	// +optional
+	DiscoveryServiceType corev1.ServiceType `json:"discoveryServiceType"`
+
+	// Method of how each member is accessed from the external client.
+	// +kubebuilder:default:=NodeExternalIP
+	// +kubebuilder:validation:Enum=NodeExternalIP;NodeName;LoadBalancer
+	// +optional
+	MemberAccess string `json:"memberAccess"`
 }
 
 // HazelcastStatus defines the observed state of Hazelcast
