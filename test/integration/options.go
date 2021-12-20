9d9fc6f2ad4d6b1a464f3a6d847edce60bf64042
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2021-12-20 11:43:45 +0300

    [no-ci] Increase test timeouts

--- test/integration/options.go
+++ test/integration/options.go
@@ -14,5 +14,5 @@ var (
 func init() {
 	flag.BoolVar(&ee, "ee", false, "Flag to define whether Enterprise edition of Hazelcast will be used")
 	flag.DurationVar(&interval, "interval", time.Millisecond*250, "The length of time between checks")
-	flag.DurationVar(&timeout, "eventually-timeout", time.Second*1, "Timeout for test steps")
+	flag.DurationVar(&timeout, "eventually-timeout", time.Second*5, "Timeout for test steps")
 }
