e08cc66f93e0a258286c9b7aba5fa9080f7edc3d
Hakan Memisoglu <hakanmemisoglu@gmail.com>
2022-02-15 12:24:18 +0300

    Add Turbine CR

---
+++ api/v1alpha1/turbine_types.go
@@ -0,0 +1,73 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// TurbineSpec defines the desired state of Turbine
+type TurbineSpec struct {
+	// Sidecar configuration
+	Sidecar SidecarConfiguration `json:"sidecar"`
+
+	// Hazelcast cluster to connect
+	Hazelcast *HazelcastReference `json:"hazelcast,omitempty"`
+
+	// Pod configuration
+	Pods *PodConfiguration `json:"pods,omitempty"`
+}
+
+type SidecarConfiguration struct {
+	// Name of the Turbine sidecar container
+	// +kubebuilder:default:="turbine-sidecar"
+	Name string `json:"name"`
+
+	// Image used for Turbine sidecar container
+	// +kubebuilder:default:="hazelcast/turbine-sidecar"
+	Image string `json:"image"`
+}
+
+type HazelcastReference struct {
+	// Address of the Hazelcast cluster
+	ClusterAddress *string `json:"clusterAddress,omitempty"`
+
+	// Reference to Hazelcast CR
+	Cluster *HazelcastRef `json:"cluster,omitempty"`
+}
+
+type HazelcastRef struct {
+	// Name of Hazelcast CR
+	Name string `json:"name"`
+
+	// Namespace of Hazelcast CR
+	// +kubebuilder:default:="default"
+	Namespace string `json:"namespace"`
+}
+
+type PodConfiguration struct {
+	// The name of the container port that Turbine will register
+	AppPortName *string `json:"portName,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:resource:scope=Cluster
+
+// Turbine is the Schema for the turbines API
+type Turbine struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec TurbineSpec `json:"spec,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// TurbineList contains a list of Turbine
+type TurbineList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Turbine `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Turbine{}, &TurbineList{})
+}
--- controllers/turbine/injector.go
+++ controllers/turbine/injector.go
@@ -10,27 +10,25 @@ import (
 
 	"github.com/go-logr/logr"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	hazelcastv1alpha1 "github.com/hazelcast/hazelcast-platform-operator/api/v1alpha1"
 )
 
 const (
 	injectedLabelKey   = "turbine.hazelcast.com/injected"
 	injectedLabelValue = "true"
 
-	injectionEnabledLabelKey   = "turbine.hazelcast.com/enabled"
-	injectionEnabledLabelValue = "true"
+	turbineNameLabelKey = "turbine.hazelcast.com/name"
 
-	configMapNameAnnotationKey = "turbine.hazelcast.com/configmap"
-	appPortAnnotationKey       = "turbine.hazelcast.com/app-port"
+	appPortAnnotationKey        = "turbine.hazelcast.com/app-port"
+	clusterAddressAnnotationKey = "turbine.hazelcast.com/cluster-address"
 
-	sidecarName  = "turbine-sidecar"
-	sidecarImage = "hazelcast/turbine-sidecar"
-
-	envPodIp       = "TURBINE_POD_IP"
-	envAppHttpPort = "APP_HTTP_PORT"
-
-	appHttpPortName = "app-http"
+	envPodIp          = "TURBINE_POD_IP"
+	envAppHttpPort    = "APP_HTTP_PORT"
+	envClusterAddress = "CLUSTER_ADDRESS"
 )
 
 type Injector struct {
@@ -44,6 +42,11 @@ func New(cli client.Client, logger logr.Logger, ns string) *Injector {
 	return &Injector{client: cli, logger: logger, namespace: ns}
 }
 
+func (i *Injector) InjectDecoder(d *admission.Decoder) error {
+	i.decoder = d
+	return nil
+}
+
 // +kubebuilder:webhook:path=/inject-turbine,mutating=true,sideEffects="None",failurePolicy=fail,groups="",resources=pods,verbs=create;update,versions=v1,admissionReviewVersions=v1,name=inject-turbine.hazelcast.com
 
 func (i *Injector) Handle(ctx context.Context, req admission.Request) admission.Response {
@@ -55,58 +58,31 @@ func (i *Injector) Handle(ctx context.Context, req admission.Request) admission.
 	name, namespace := inferMetadata(req, pod)
 	logger := i.logger.WithValues("podName", name, "podNamespace", namespace)
 
-	// Not needed since the object selection in webhook configuration, we are doing it anyway.
-	if !hasInjectionEnabled(pod) {
-		logger.Error(errors.New("webhook received pod with unexpected condition"), "unexpected pod label condition")
-		return admission.Allowed(
-			fmt.Sprintf(
-				"pod {name: %s, namespace: %s} is not labeled",
-				name,
-				namespace,
-			))
+	turbine, err := i.getTurbineConfig(ctx, pod)
+	if err != nil {
+		logger.Error(err, "failed to get Turbine CR")
+		return admission.Errored(http.StatusBadRequest, err)
 	}
 
-	status := getInjectionStatus(pod)
-
-	switch status {
-	case LabeledWithSidecar:
-		return admission.Allowed(
-			fmt.Sprintf(
-				"pod {name: %s, namespace: %s} is already injected with turbine sidecar",
-				name,
-				namespace,
-			))
-
-	case LabeledWithoutSidecar:
-		logger.Error(errors.New("pod was labeled before but it was not injected"), "unexpected condition")
-		pod = injectTurbineSidecar(pod)
-
-	case NoLabelWithSidecar:
-		logger.Error(errors.New("pod was injected by another entity"), "unexpected condition")
-		pod = addInjectedLabel(pod)
-
-	case NoLabelWithoutSidecar:
-		pod = injectTurbineSidecar(pod)
+	pod, err = i.injectSidecar(ctx, pod, turbine)
+	if err != nil {
+		logger.Error(err, "failed to inject Turbine sidecar")
+		return admission.Errored(http.StatusBadRequest, err)
 	}
 
 	if result, err := json.Marshal(pod); err == nil {
 		logger.Info("pod is patched successfully")
 		return admission.PatchResponseFromRaw(req.Object.Raw, result)
 	} else {
-		i.logger.Error(err, "unable to marshal result pod object")
+		logger.Error(err, "unable to marshal result pod object")
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 }
 
-func (i *Injector) InjectDecoder(d *admission.Decoder) error {
-	i.decoder = d
-	return nil
-}
-
-func injectTurbineSidecar(pod *v1.Pod) *v1.Pod {
+func (i *Injector) injectSidecar(ctx context.Context, pod *v1.Pod, turbine *hazelcastv1alpha1.Turbine) (*v1.Pod, error) {
 	sidecar := v1.Container{
-		Name:  sidecarName,
-		Image: sidecarImage,
+		Name:  turbine.Spec.Sidecar.Name,
+		Image: turbine.Spec.Sidecar.Image,
 		Env: []v1.EnvVar{
 			{
 				Name: envPodIp,
@@ -119,71 +95,89 @@ func injectTurbineSidecar(pod *v1.Pod) *v1.Pod {
 		},
 	}
 
-	port := getAppPort(pod)
-	if port != "" {
+	if port := getAppPort(pod, turbine); port != "" {
 		sidecar.Env = append(sidecar.Env, v1.EnvVar{
 			Name:  envAppHttpPort,
 			Value: port,
 		})
 	}
 
-	cm := getConfigMapName(pod)
-	if cm != "" {
-		sidecar.EnvFrom = append(sidecar.EnvFrom, envFromConfigmap(cm))
+	if addr := i.getClusterAddress(ctx, pod, turbine); addr != "" {
+		sidecar.Env = append(sidecar.Env, v1.EnvVar{
+			Name:  envClusterAddress,
+			Value: addr,
+		})
 	}
 
 	pod.Spec.Containers = append(pod.Spec.Containers, sidecar)
+	addInjectedLabel(pod)
 
-	pod = addInjectedLabel(pod)
-	return pod
+	return pod, nil
+}
+
+func (i *Injector) getTurbineConfig(ctx context.Context, p *v1.Pod) (*hazelcastv1alpha1.Turbine, error) {
+	crName := p.Labels[turbineNameLabelKey]
+	if crName == "" {
+		return nil, errors.New(turbineNameLabelKey + " value is empty")
+	}
+	cr := &hazelcastv1alpha1.Turbine{}
+	err := i.client.Get(ctx, types.NamespacedName{Name: crName}, cr)
+	return cr, err
+}
+
+func (i *Injector) getClusterAddress(ctx context.Context, p *v1.Pod, turbine *hazelcastv1alpha1.Turbine) string {
+	// Annotation takes precedence
+	if addr, ok := p.Annotations[clusterAddressAnnotationKey]; ok && addr != "" {
+		return addr
+	}
+
+	// If it fails, fallback to Turbine CR
+	hzConf, addr := turbine.Spec.Hazelcast, ""
+	if hzConf != nil && hzConf.ClusterAddress != nil {
+		addr = *hzConf.ClusterAddress
+	}
+	if hzConf != nil && hzConf.Cluster != nil {
+		addr = i.getClusterAddressFromCR(ctx, hzConf.Cluster.Name, hzConf.Cluster.Namespace)
+	}
+	return addr
+}
+
+func (i *Injector) getClusterAddressFromCR(ctx context.Context, name string, namespace string) string {
+	svc := &v1.Service{}
+	err := i.client.Get(ctx, types.NamespacedName{Name: name, Namespace: namespace}, svc)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%s:%d", svc.Spec.ClusterIP, svc.Spec.Ports[0].Port)
+}
+
+func getAppPort(p *v1.Pod, turbine *hazelcastv1alpha1.Turbine) string {
+	// Annotation takes precedence
+	if port, ok := p.Annotations[appPortAnnotationKey]; ok && port != "" {
+		return port
+	}
+
+	// If annotation is not there, fallback to Turbine CR
+	podsConf, portName := turbine.Spec.Pods, ""
+	if podsConf != nil && podsConf.AppPortName != nil {
+		portName = *podsConf.AppPortName
+	}
+	if portName == "" {
+		return ""
+	}
+	for i := range p.Spec.Containers {
+		for j := range p.Spec.Containers[i].Ports {
+			if p.Spec.Containers[i].Ports[j].Name == portName {
+				return strconv.FormatInt(int64(p.Spec.Containers[i].Ports[j].ContainerPort), 10)
+			}
+		}
+	}
+	return ""
 }
 
 func addInjectedLabel(p *v1.Pod) *v1.Pod {
 	p.Labels[injectedLabelKey] = injectedLabelValue
 	return p
-}
-
-type injectionStatus int
-
-const (
-	LabeledWithSidecar = iota
-	LabeledWithoutSidecar
-	NoLabelWithSidecar
-	NoLabelWithoutSidecar
-)
-
-func getInjectionStatus(p *v1.Pod) injectionStatus {
-	sideCarExists := hasTurbineSidecar(p)
-	labeled := isLabeledAsInjected(p)
-
-	if sideCarExists && labeled {
-		return LabeledWithSidecar
-	} else if sideCarExists {
-		return NoLabelWithSidecar
-	} else if labeled {
-		return LabeledWithoutSidecar
-	} else {
-		return NoLabelWithoutSidecar
-	}
-}
-
-func hasTurbineSidecar(p *v1.Pod) bool {
-	for _, c := range p.Spec.Containers {
-		if c.Name == sidecarName && c.Image == sidecarImage {
-			return true
-		}
-	}
-	return false
-}
-
-func isLabeledAsInjected(p *v1.Pod) bool {
-	val, ok := p.Labels[injectedLabelKey]
-	return ok && val == injectedLabelValue
-}
-
-func hasInjectionEnabled(p *v1.Pod) bool {
-	val, ok := p.Labels[injectionEnabledLabelKey]
-	return ok && val == injectionEnabledLabelValue
 }
 
 func inferMetadata(req admission.Request, pod *v1.Pod) (name string, namespace string) {
@@ -192,7 +186,7 @@ func inferMetadata(req admission.Request, pod *v1.Pod) (name string, namespace s
 	} else if req.Name != "" {
 		name = req.Name
 	} else {
-		name = pod.GenerateName + "${RANDOM}"
+		name = pod.GenerateName + "<RANDOM>"
 	}
 
 	if req.Namespace != "" {
@@ -202,34 +196,4 @@ func inferMetadata(req admission.Request, pod *v1.Pod) (name string, namespace s
 	}
 
 	return
-}
-
-func envFromConfigmap(name string) v1.EnvFromSource {
-	return v1.EnvFromSource{
-		ConfigMapRef: &v1.ConfigMapEnvSource{
-			LocalObjectReference: v1.LocalObjectReference{
-				Name: name,
-			},
-		},
-	}
-}
-
-func getConfigMapName(p *v1.Pod) string {
-	return p.Annotations[configMapNameAnnotationKey]
-}
-
-func getAppPort(p *v1.Pod) string {
-	if port, ok := p.Annotations[appPortAnnotationKey]; ok && port != "" {
-		return port
-	}
-
-	for i := range p.Spec.Containers {
-		for j := range p.Spec.Containers[i].Ports {
-			if p.Spec.Containers[i].Ports[j].Name == appHttpPortName {
-				return strconv.FormatInt(int64(p.Spec.Containers[i].Ports[j].ContainerPort), 10)
-			}
-		}
-	}
-
-	return ""
 }
