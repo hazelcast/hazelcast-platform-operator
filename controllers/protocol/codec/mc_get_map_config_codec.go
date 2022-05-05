601152962f9674e09c57adc6c25932b6643fde33
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-05-05 13:48:03 +0300

    Add indexes into DecodeMCGetMapConfigResponse

--- controllers/protocol/codec/mc_get_map_config_codec.go
+++ controllers/protocol/codec/mc_get_map_config_codec.go
@@ -70,16 +70,20 @@ func DecodeMCGetMapConfigResponse(clientMessage *proto.ClientMessage) types.MapC
 		ReadBackupData:    DecodeBoolean(initialFrame.Content, MCGetMapConfigResponseReadBackupDataOffset),
 		EvictionPolicy:    DecodeInt(initialFrame.Content, MCGetMapConfigResponseEvictionPolicyOffset),
 		MergePolicy:       DecodeString(frameIterator),
+		Indexes:           DecodeListMultiFrameForIndexConfig(frameIterator),
 	}
 	return mc
 }
 
 func DecodeListMultiFrameForIndexConfig(frameIterator *proto.ForwardFrameIterator) []types.IndexConfig {
 	result := make([]types.IndexConfig, 0)
-	frameIterator.Next()
-	for NextFrameIsDataStructureEndFrame(frameIterator) {
-		result = append(result, DecodeIndexConfig(frameIterator))
+	if frameIterator.HasNext() {
+		frameIterator.Next()
+
+		for !NextFrameIsDataStructureEndFrame(frameIterator) {
+			result = append(result, DecodeIndexConfig(frameIterator))
+		}
+		frameIterator.Next()
 	}
-	frameIterator.Next()
 	return result
 }
