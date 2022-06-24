2209071780933b7b7a328d9c9043c7da56b8363d
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-06-24 10:34:41 +0300

    Remove eventually-timeout from e2e tests

--- test/e2e/options_test.go
+++ test/e2e/options_test.go
@@ -8,7 +8,6 @@ import (
 var (
 	hzNamespace   string
 	interval      time.Duration
-	timeout       time.Duration
 	deleteTimeout time.Duration
 	ee            bool
 )
@@ -16,7 +15,6 @@ var (
 func init() {
 	flag.StringVar(&hzNamespace, "namespace", "default", "The namespace to run e2e tests")
 	flag.DurationVar(&interval, "interval", 1*time.Second, "The length of time between checks")
-	flag.DurationVar(&timeout, "eventually-timeout", 5*time.Minute, "Timeout for test steps")
 	flag.DurationVar(&deleteTimeout, "delete-timeout", 5*time.Minute, "Timeout for resource deletions")
 	flag.BoolVar(&ee, "ee", true, "Flag to define whether Enterprise edition of Hazelcast will be used")
 }
