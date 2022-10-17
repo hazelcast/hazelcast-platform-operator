5caa231b059de90eeeb3dc23273a6d45d7f796bc
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-10-17 20:37:09 +0300

    Call addBatchPublisher on random target
    This method does not need to be called on all members. Sending it to
    one member is enough.

--- controllers/hazelcast/wanreplication_controller.go
+++ controllers/hazelcast/wanreplication_controller.go
@@ -391,12 +391,11 @@ func addBatchPublisherConfig(
 		request.queueFullBehavior,
 	)
 
-	for _, member := range cliInt.OrderedMembers() {
-		_, err := cliInt.InvokeOnMember(ctx, req, member.UUID, nil)
-		if err != nil {
-			return err
-		}
+	_, err := cliInt.InvokeOnRandomTarget(ctx, req, nil)
+	if err != nil {
+		return err
 	}
+
 	return nil
 }
 
