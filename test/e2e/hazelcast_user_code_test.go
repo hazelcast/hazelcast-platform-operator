f02c9aede39176726a7be44e0f1b7dd845277833
Serge Bishyr <seriybg@gmail.com>
2022-10-25 12:22:25 +0300

    clean up

--- test/e2e/hazelcast_user_code_test.go
+++ test/e2e/hazelcast_user_code_test.go
@@ -177,7 +177,7 @@ var _ = Describe("Hazelcast User Code Deployment", Label("custom_class"), func()
 		assertExecutorServices(sampleExecutorServices, actualES)
 	})
 
-	FIt("should add entry listeners", Label("fast"), func() {
+	It("should add entry listeners", Label("fast"), func() {
 		setLabelAndCRName("hel-1")
 
 		h := hazelcastconfig.UserCode(hzLookupKey, ee, "br-secret-gcp", "gs://operator-user-code/entryListener", labels)
