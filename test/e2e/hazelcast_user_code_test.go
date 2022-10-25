e604849db5066f40625d908353a557ff9da6deeb
Serge Bishyr <seriybg@gmail.com>
2022-10-25 13:23:14 +0300

    update test

--- test/e2e/hazelcast_user_code_test.go
+++ test/e2e/hazelcast_user_code_test.go
@@ -182,6 +182,7 @@ var _ = Describe("Hazelcast User Code Deployment", Label("custom_class"), func()
 
 		h := hazelcastconfig.UserCode(hzLookupKey, ee, "br-secret-gcp", "gs://operator-user-code/entryListener", labels)
 		CreateHazelcastCR(h)
+		t := Now()
 
 		By("creating map with Map with entry listener")
 		ms := hazelcastcomv1alpha1.MapSpec{
@@ -195,7 +196,6 @@ var _ = Describe("Hazelcast User Code Deployment", Label("custom_class"), func()
 		m := hazelcastconfig.Map(ms, mapLookupKey, labels)
 		Expect(k8sClient.Create(context.Background(), m)).Should(Succeed())
 		assertMapStatus(m, hazelcastcomv1alpha1.MapSuccess)
-		t := Now()
 
 		By("port-forwarding to Hazelcast master pod")
 		stopChan := portForwardPod(hazelcastconfig.UserCode(hzLookupKey, ee, "br-secret-gcp", "gs://operator-user-code/entryListener", labels).Name+"-0", hazelcastconfig.UserCode(hzLookupKey, ee, "br-secret-gcp", "gs://operator-user-code/mapStore", labels).Namespace, localPort+":5701")
