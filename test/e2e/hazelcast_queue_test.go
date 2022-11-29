74661624a66dc7a8514b6a179cee4eec555fc42d
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-11-29 19:52:04 +0300

    Remove duplicated imports

--- test/e2e/hazelcast_queue_test.go
+++ test/e2e/hazelcast_queue_test.go
@@ -5,8 +5,6 @@ import (
 	"strconv"
 	. "time"
 
-	n "github.com/hazelcast/hazelcast-platform-operator/internal/naming"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
