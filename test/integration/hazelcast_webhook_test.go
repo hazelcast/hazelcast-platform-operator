7fea6bb4e1d36df398e10c848a48b5775e8ad325
Hasan Çelik <hasan@hazelcast.com>
2023-01-05 23:01:32 +0300

    fix import

--- test/integration/hazelcast_webhook_test.go
+++ test/integration/hazelcast_webhook_test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/uuid"
 
 	hazelcastv1alpha1 "github.com/hazelcast/hazelcast-platform-operator/api/v1alpha1"
