0e1027acb257ad3610b2fb3b17a564e2191db319
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2023-02-01 16:42:10 +0300

    Update api/v1alpha1/hazelcast_validation.go

--- api/v1alpha1/hazelcast_validation.go
+++ api/v1alpha1/hazelcast_validation.go
@@ -92,7 +92,7 @@ func validateLicense(h *Hazelcast) error {
 		err := kubeclient.Get(context.Background(), secretName, &secret)
 		if kerrors.IsNotFound(err) {
 			// we care only about not found error
-			return errors.New("Hazelcast Enterprise licenseKeySecret secret is not found")
+			return errors.New("Hazelcast Enterprise licenseKeySecret is not found")
 		}
 	}
 
