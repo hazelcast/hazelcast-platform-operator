f1c42b042795d254d04c65947e15342373a58b79
aborschuk <alexander.borschuk@hazelcast.com>
2023-02-14 23:04:53 +0100

    remove F

--- test/e2e/hazelcast_backup_slow_test.go
+++ test/e2e/hazelcast_backup_slow_test.go
@@ -410,7 +410,7 @@ var _ = Describe("Hazelcast Backup", Label("backup_slow"), func() {
 		WaitForMapSize(context.Background(), hzLookupKey, m.Name, 100, 10*Minute)
 	})
 
-	FIt("should not start repartitioning after planned shutdown", Label("slow"), func() {
+	It("should not start repartitioning after planned shutdown", Label("slow"), func() {
 		if !ee {
 			Skip("This test will only run in EE configuration")
 		}
