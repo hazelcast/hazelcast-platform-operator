f6a5abd50b7f0c9bbf1074fbe99dc59b8c35c797
Muhammet Yazici <44066377+mtyazici@users.noreply.github.com>
2023-02-15 10:28:25 +0300

    Add phonehome for watched namespace type (#616)

--- internal/util/util.go
+++ internal/util/util.go
@@ -13,9 +13,6 @@ import (
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/uuid"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
@@ -174,49 +171,6 @@ func IsDeveloperModeEnabled() bool {
 	return strings.ToLower(value) == "true"
 }
 
-func GetOperatorVersion() string {
-	return os.Getenv(n.OperatorVersionEnv)
-}
-
-func GetPardotID() string {
-	return os.Getenv(n.PardotIDEnv)
-}
-
-func GetOperatorID(c *rest.Config) types.UID {
-	uid, err := getOperatorDeploymentUID(c)
-	if err == nil {
-		return uid
-	}
-
-	return uuid.NewUUID()
-}
-
-func getOperatorDeploymentUID(c *rest.Config) (types.UID, error) {
-	ns, okNS := os.LookupEnv(n.NamespaceEnv)
-	podName, okPN := os.LookupEnv(n.PodNameEnv)
-
-	if !okNS || !okPN {
-		return "", fmt.Errorf("%s or %s is not defined", n.NamespaceEnv, n.PodNameEnv)
-	}
-	client, err := kubernetes.NewForConfig(c)
-	if err != nil {
-		return "", err
-	}
-
-	var d *appsv1.Deployment
-	d, err = client.AppsV1().Deployments(ns).Get(context.TODO(), DeploymentName(podName), metav1.GetOptions{})
-	if err != nil {
-		return "", err
-	}
-
-	return d.UID, nil
-}
-
-func DeploymentName(podName string) string {
-	s := strings.Split(podName, "-")
-	return strings.Join(s[:len(s)-2], "-")
-}
-
 type ExternalAddresser interface {
 	metav1.Object
 	ExternalAddressEnabled() bool
@@ -295,10 +249,6 @@ func IsApplied(obj client.Object) bool {
 func IsSuccessfullyApplied(obj client.Object) bool {
 	_, ok := obj.GetAnnotations()[n.LastSuccessfulSpecAnnotation]
 	return ok
-}
-
-func IsWatchingAllNamespaces(ns string) bool {
-	return ns == "" || ns == "*"
 }
 
 func NodeDiscoveryEnabled() bool {
