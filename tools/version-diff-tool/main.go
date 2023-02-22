00945bff85be2cd2dd4ea336863c723313f2b90b
kmetin <kutluhan.metin@hazelcast.com>
2023-02-22 12:48:28 +0300

    complete version diff tool

--- tools/version-diff-tool/main.go
+++ tools/version-diff-tool/main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/hashicorp/go-version"
 	"github.com/tidwall/sjson"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"sigs.k8s.io/yaml"
 	"strings"
 )
 
 const (
+	// File names
+	outputFileName          = `diff_%s_%s.txt`
+	descriptionlessFileName = `r%s`
+	bundleFileName          = `bundle-%s.yaml`
+	openAPIFileName         = `open-api-%s`
+
+	// Splitters
+	schemaSplitter = `---`
+
+	// Links
+	bundleDownloadLink = `https://repository.hazelcast.com/operator/` + bundleFileName
+
+	// Helm related
+	helmInstall      = `helm install operator hazelcast/hazelcast-platform-operator --dry-run --set installCRDs=true > ` + bundleFileName
+	helmChartVersion = "5.6"
+
+	// yq commands
 	crdNamesSelector   = `yq '.. | select(has("singular")).singular' %s`
 	schemaSelector     = `yq '.. | select(has("schema")).schema' %s`
-	descriptionDeleter = `yq 'del(.. | select(has("description")).description)' %s > r%s`
-	openAPIYAML        = `openapi: 3.0.0
+	descriptionDeleter = `yq 'del(.. | select(has("description")).description)' %s > ` + descriptionlessFileName
+	flatten            = `yq eval '.. | select((tag == "!!map" or tag == "!!seq") | not) | (path | join(".")) + "=" + .' kdiff.yaml | sed 's/components.schemas.//g' | sed 's/properties.//g' | sed 's/modified.//g' | sed 's/stringsdiff.//g'`
+
+	// oasdiff commands
+	diff = `oasdiff -base %s -revision %s > kdiff.yaml`
+
+	// Open API YAML
+	openAPIYAML = `openapi: 3.0.0
 info:
   version: 1.0.0
   title: Sample API
@@ -23,231 +49,249 @@ info:
 components:
   schemas:
 `
-	diff    = `oasdiff -base %s -revision %s > kdiff.yaml`
-	flatten = `yq eval '.. | select((tag == "!!map" or tag == "!!seq") | not) | (path | join(".")) + "=" + .' kdiff.yaml`
 )
 
 func main() {
-
-	cmd := exec.Command("git", "for-each-ref", `--format='%(refname)'`, "refs/tags")
-	out, err := cmd.CombinedOutput()
+	base, revision, err := parseParams()
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("an error occurred while parsing parameters: %s", err.Error()))
 	}
 
-	tags := findTags(string(out))
-	yamlFileNames, err := downloadBundleYAMLs(tags)
+	baseBundle, err := downloadBundleYAML(base)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("an error occurred while downloading base bundle yaml: %s", err.Error()))
 	}
 
-	yamlFileNames = []string{"bundle-5.3.yaml", "bundle-5.4.yaml"}
-	_, err = createOpenAPISpec(yamlFileNames)
+	revBundle, err := downloadBundleYAML(revision)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("an error occurred while downloading revision bundle yaml: %s", err.Error()))
 	}
 
-	err = compareSpecs("open-api-bundle-5.3.yaml", "open-api-bundle-5.4.yaml")
+	baseSpec, err := createOpenAPISpec(baseBundle)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("an error occurred while creating base api spec: %s", err.Error()))
 	}
 
-	err = formatDiff()
+	revSpec, err := createOpenAPISpec(revBundle)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("an error occurred while creating revision api spec: %s", err.Error()))
 	}
+
+	diffFile, err := compareSpecs(baseSpec, revSpec)
+	if err != nil {
+		panic(fmt.Errorf("an error occurred while comparing specs: %s", err.Error()))
+	}
+
+	err = cleanup()
+	if err != nil {
+		panic(fmt.Errorf("an error occurred while cleaning up: %s", err.Error()))
+	}
+
+	fmt.Println("Spec diff is extracted to file: " + diffFile)
 }
 
-func findTags(tags string) []string {
+func parseParams() (string, string, error) {
+	var base, revision string
+
+	flag.StringVar(&base, "base", "", "base yaml")
+	flag.StringVar(&revision, "revision", "", "revision yaml")
+	flag.Parse()
+
+	if base != "" && revision != "" {
+		_, err := version.NewVersion(base)
+		if err != nil {
+			return "", "", err
+		}
+		_, err = version.NewVersion(revision)
+		if err != nil {
+			return "", "", err
+		}
+	} else {
+		cmd := exec.Command("git", "for-each-ref", `--format='%(refname)'`, "refs/tags")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return "", "", err
+		}
+
+		base, revision = findTags(string(out))
+	}
+
+	return base, revision, nil
+}
+
+func findTags(tags string) (string, string) {
 	tags = strings.ReplaceAll(tags, "'", "")
 	tags = strings.ReplaceAll(tags, "refs/tags/v", "")
 	tagList := strings.Split(tags, "\n")
 
-	var detectedTags []string
-	for i := len(tagList) - 1; i > 0; i-- {
-		if tagList[i] != "" {
-			detectedTags = append(detectedTags, tagList[i])
-		}
-		if len(detectedTags) == 2 {
-			break
-		}
-	}
+	revision := tagList[len(tagList)-1]
+	base := tagList[len(tagList)-2]
 
-	return detectedTags
+	return base, revision
 }
 
-func downloadBundleYAMLs(tags []string) ([]string, error) {
-	var fileNames []string
+func downloadBundleYAML(v string) (string, error) {
+	helmVersion, _ := version.NewVersion(helmChartVersion)
+	vv, _ := version.NewVersion(v) // versions already validated, so ignore error
 
-	for _, t := range tags {
-		//TODO: semantic version comparison (> 5.5)
-		// https://repository.hazelcast.com/operator/bundle-5.5.yaml
-		// helm install operator hazelcast/hazelcast-platform-operator --dry-run --debug --set installCRDs=true > 5.6.yaml
-
-		resp, err := http.Get(fmt.Sprintf("https://repository.hazelcast.com/operator/bundle-%s.yaml", t))
-		if err != nil {
-			return []string{}, err
-		}
-
-		f, err := os.Create(fmt.Sprintf("bundle-%s.yaml", t))
-		if err != nil {
-			return []string{}, err
-		}
-
-		_, err = io.Copy(f, resp.Body)
-		if err != nil {
-			return []string{}, err
-		}
-
-		fileNames = append(fileNames, f.Name())
-	}
-
-	return fileNames, nil
-}
-
-type parsedYAML struct {
-	fileName string
-	crdNames []string
-	schemas  []string
-}
-
-func createOpenAPISpec(yamlFiles []string) ([]string, error) {
-	var yamls []parsedYAML
-
-	for _, y := range yamlFiles {
-		cmd := exec.Command("bash", "-c", fmt.Sprintf(descriptionDeleter, y, y))
+	if vv.GreaterThanOrEqual(helmVersion) {
+		cmd := exec.Command("bash", "-c", fmt.Sprintf(helmInstall, v))
 		_, err := cmd.CombinedOutput()
 		if err != nil {
-			return []string{}, err
+			return "", err
 		}
-
-		descriptionLessFileName := "r" + y
-
-		cmd = exec.Command("bash", "-c", fmt.Sprintf(crdNamesSelector, descriptionLessFileName))
-		o2, err := cmd.CombinedOutput()
-		if err != nil {
-			return []string{}, err
-		}
-
-		cmd = exec.Command("bash", "-c", fmt.Sprintf(schemaSelector, descriptionLessFileName))
-		o3, err := cmd.CombinedOutput()
-		if err != nil {
-			return []string{}, err
-		}
-
-		p := parsedYAML{
-			fileName: y,
-			crdNames: strings.Split(string(o2), "---"),
-			schemas:  strings.Split(string(o3), "---"),
-		}
-
-		// clean up some fields to make them the same format
-		for i := range p.crdNames {
-			p.crdNames[i] = strings.ReplaceAll(p.crdNames[i], "\n", "")
-		}
-
-		for i := range p.schemas {
-			p.schemas[i] = strings.ReplaceAll(p.schemas[i], "openAPIV3Schema:\n", "")
-		}
-
-		yamls = append(yamls, p)
+		return fmt.Sprintf(bundleFileName, v), nil
 	}
 
-	mergeCRDNamesAndSchemas(yamls)
+	resp, err := http.Get(fmt.Sprintf(bundleDownloadLink, v))
+	if err != nil {
+		return "", err
+	}
 
-	return []string{}, nil
+	f, err := os.Create(fmt.Sprintf(bundleFileName, v))
+	if err != nil {
+		return "", err
+	}
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return f.Name(), nil
 }
 
-func mergeCRDNamesAndSchemas(yamls []parsedYAML) error {
-	specs := make(map[string][]byte) // fileName -> openAPISpec YAML
-
-	for _, y := range yamls {
-		var wholeSchema []byte
-		for i := range y.schemas {
-			s, err := yaml.YAMLToJSON([]byte(y.schemas[i]))
-			if err != nil {
-				return err
-			}
-
-			wholeSchema, err = sjson.SetRawBytes(wholeSchema, y.crdNames[i], s)
-			if err != nil {
-				return err
-			}
-		}
-
-		specs[y.fileName] = wholeSchema
-	}
-
-	j1, err := yaml.YAMLToJSON([]byte(openAPIYAML))
-	if err != nil {
-		return err
-	}
-
-	json1, err := sjson.SetRawBytes(j1, "components.schemas", specs[yamls[0].fileName])
-	if err != nil {
-		return err
-	}
-
-	yaml1, err := yaml.JSONToYAML(json1)
-	if err != nil {
-		return err
-	}
-
-	file1, err := os.Create("open-api-" + yamls[0].fileName)
-	if err != nil {
-		return err
-	}
-	defer file1.Close()
-	_, err = file1.WriteString(string(yaml1))
-	if err != nil {
-		return err
-	}
-
-	j2, err := yaml.YAMLToJSON([]byte(openAPIYAML))
-	if err != nil {
-		return err
-	}
-
-	json2, err := sjson.SetRawBytes(j2, "components.schemas", specs[yamls[1].fileName])
-	if err != nil {
-		return err
-	}
-
-	yaml2, err := yaml.JSONToYAML(json2)
-	if err != nil {
-		return err
-	}
-
-	file2, err := os.Create("open-api-" + yamls[1].fileName)
-	if err != nil {
-		return err
-	}
-	defer file2.Close()
-	_, err = file2.WriteString(string(yaml2))
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func compareSpecs(file1, file2 string) error {
-	cmd := exec.Command("bash", "-c", fmt.Sprintf(diff, file1, file2))
+func createOpenAPISpec(yaml string) (string, error) {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf(descriptionDeleter, yaml, yaml))
 	_, err := cmd.CombinedOutput()
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+
+	cmd = exec.Command("bash", "-c", fmt.Sprintf(crdNamesSelector, fmt.Sprintf(descriptionlessFileName, yaml)))
+	o1, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	cmd = exec.Command("bash", "-c", fmt.Sprintf(schemaSelector, fmt.Sprintf(descriptionlessFileName, yaml)))
+	o2, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	crdNames := strings.Split(string(o1), schemaSplitter)
+	for i := range crdNames {
+		crdNames[i] = strings.ReplaceAll(crdNames[i], "\n", "")
+	}
+
+	schemas := strings.Split(string(o2), schemaSplitter)
+	for i := range schemas {
+		schemas[i] = strings.ReplaceAll(schemas[i], "openAPIV3Schema:\n", "")
+	}
+
+	n, err := mergeCRDNamesAndSchemas(yaml, crdNames, schemas)
+	if err != nil {
+		return "", err
+	}
+
+	return n, nil
 }
 
-func formatDiff() error {
+func mergeCRDNamesAndSchemas(fileName string, crdNames, schemas []string) (string, error) {
+	var wholeSchema []byte
+	for i := range schemas {
+		s, err := yaml.YAMLToJSON([]byte(schemas[i]))
+		if err != nil {
+			return "", err
+		}
+
+		wholeSchema, err = sjson.SetRawBytes(wholeSchema, crdNames[i], s)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	j, err := yaml.YAMLToJSON([]byte(openAPIYAML))
+	if err != nil {
+		return "", err
+	}
+
+	js, err := sjson.SetRawBytes(j, "components.schemas", wholeSchema)
+	if err != nil {
+		return "", err
+	}
+
+	y, err := yaml.JSONToYAML(js)
+	if err != nil {
+		return "", err
+	}
+
+	f, err := os.Create(fmt.Sprintf(openAPIFileName, fileName))
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	_, err = f.WriteString(string(y))
+	if err != nil {
+		return "", err
+	}
+
+	return f.Name(), nil
+}
+
+func compareSpecs(base, rev string) (string, error) {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf(diff, base, rev))
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+
+	diffFile, err := formatDiff(base, rev)
+	if err != nil {
+		return "", err
+	}
+	return diffFile, nil
+}
+
+func formatDiff(base, rev string) (string, error) {
 	cmd := exec.Command("bash", "-c", flatten)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
+		return "", err
+	}
+
+	f, err := os.Create(fmt.Sprintf(outputFileName, base, rev))
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(string(output))
+	if err != nil {
+		return "", err
+	}
+
+	return f.Name(), nil
+}
+
+func cleanup() error {
+	wd, err := os.Getwd()
+	if err != nil {
 		return err
 	}
 
-	fmt.Println(output)
-
+	err = filepath.Walk(wd, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) == ".yaml" {
+			os.Remove(info.Name())
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
 	return nil
 }
