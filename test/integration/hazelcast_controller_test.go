59e9a9f8ef27143fddbf7249aed355326207297d
semihbkgr <buyukgungorsemih@gmail.com>
2023-03-20 11:35:42 +0300

    wanreplication integration test for endpoint ports

--- test/integration/hazelcast_controller_test.go
+++ test/integration/hazelcast_controller_test.go
@@ -947,6 +947,37 @@ var _ = Describe("Hazelcast controller", func() {
 		})
 	})
 
+	Context("WanReplication CR configuration", func() {
+		When("endpoints are configured without port", func() {
+			It("should set default port to endpoints", Label("fast"), func() {
+				endpoints := "10.0.0.1,10.0.0.2,10.0.0.3"
+				wan := &hazelcastv1alpha1.WanReplication{
+					ObjectMeta: GetRandomObjectMeta(),
+					Spec: hazelcastv1alpha1.WanReplicationSpec{
+						Resources: []hazelcastv1alpha1.ResourceSpec{
+							{
+								Name: "hazelcast",
+								Kind: hazelcastv1alpha1.ResourceKindHZ,
+							},
+						},
+						TargetClusterName: "dev",
+						Endpoints:         endpoints,
+					},
+				}
+				By("creating WanReplication CR successfully")
+				Expect(k8sClient.Create(context.Background(), wan)).Should(Succeed())
+				Eventually(func() string {
+					newWan := hazelcastv1alpha1.WanReplication{}
+					err := k8sClient.Get(context.Background(), types.NamespacedName{Name: wan.Name, Namespace: wan.Namespace}, &newWan)
+					if err != nil {
+						return ""
+					}
+					return newWan.Spec.Endpoints
+				}, timeout, interval).Should(Equal("10.0.0.1:5701,10.0.0.2:5701,10.0.0.3:5701"))
+			})
+		})
+	})
+
 	Context("Resources context", func() {
 		When("Resources are used", func() {
 			It("should be set to Container spec", Label("fast"), func() {
