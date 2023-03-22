dcfda0035b75dba239591a8b959361ec69160c91
semihbkgr <buyukgungorsemih@gmail.com>
2023-03-22 22:37:26 +0300

    cleanup namespace script

---
+++ .github/scripts/cleanup-namespace.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+numberOfAllResources() {
+  # number of all resources excepting `kubernetes` svc
+  kubectl get all --namespace="$1" -o json | \
+    jq '.items | map(select((.kind | contains("Service")) and (.metadata.name | contains("kubernetes")) | not)) | length'
+}
+
+numberOfPvc() {
+  # number of PVCs
+  kubectl get pvc --namespace="$1" -o json | \
+    jq '.items | length'
+}
+
+listOfBoundedPv(){
+  # space separated list of PVs which are bounded to PVCs in given namespace
+  kubectl get pvc --namespace="$1" -o json | \
+    jq -r '.items[].spec.volumeName' | \
+    tr '\n' ' '
+}
+
+ns="$1"
+
+if [ -z "$ns" ];
+then
+    echo "Namespace is not passed"
+    exit 1
+fi
+
+yellow='\033[1;33m'
+cyan='\033[0;36m'
+green='\033[1;32m'
+
+echo "${yellow}namespace: '$ns'"
+
+while [ "$(numberOfAllResources "$ns")" -gt 0 ]
+do
+  echo "${cyan}kubectl delete all${green}"
+  kubectl delete all --all --namespace="$ns"
+  sleep 3
+done
+
+if [ "$(numberOfPvc "$ns")" -gt 0 ];
+then
+  pvList="$(listOfBoundedPv "$ns")"
+  echo "${cyan}kubectl delete PVCs${green}"
+  kubectl delete pvc --all --namespace="$ns"
+  echo "${cyan}kubectl delete PV $pvList${green}"
+  kubectl delete pv $pvList || true
+fi
+
+echo '\033[0m'
