7477602ca33d95369cf220b08ccc84a9b41c89fd
semihbkgr <buyukgungorsemih@gmail.com>
2023-06-05 22:13:02 +0300

    add initial snapshot config field to jet job

--- api/v1alpha1/hazelcast_validation.go
+++ api/v1alpha1/hazelcast_validation.go
@@ -229,6 +229,8 @@ func validatePersistence(h *Hazelcast) []*field.Error {
 			"PartialStart can be used only with Partial clusterDataRecoveryPolicy"))
 	}
 
+	field.Invalid(field.NewPath("spec").Child("persistence").Child("asd"), "sad", "asd")
+
 	if len(allErrs) == 0 {
 		return nil
 	}
