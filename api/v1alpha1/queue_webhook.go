ba83ff06b56291b0324ae199e9ea6464f602ac60
dzeromski-hazelcast <102033346+dzeromski-hazelcast@users.noreply.github.com>
2023-06-07 09:47:55 +0200

    [CN-875] Add backupCount/asyncBackupCount validation in Queue (#753)

--- api/v1alpha1/queue_webhook.go
+++ api/v1alpha1/queue_webhook.go
@@ -16,9 +16,6 @@ func (r *Queue) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-hazelcast-com-v1alpha1-queue,mutating=false,failurePolicy=ignore,sideEffects=None,groups=hazelcast.com,resources=queues,verbs=create;update,versions=v1alpha1,name=vqueue.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &Queue{}
@@ -26,9 +23,7 @@ var _ webhook.Validator = &Queue{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Queue) ValidateCreate() error {
 	queuelog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return r.ValidateSpecCreate()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
@@ -40,7 +35,5 @@ func (r *Queue) ValidateUpdate(old runtime.Object) error {
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *Queue) ValidateDelete() error {
 	queuelog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
