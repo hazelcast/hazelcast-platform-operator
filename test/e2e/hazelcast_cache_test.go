ebf230525fc8cdffa7776ffc3c0fd3c5db76e4e1
cagric0 <cagri.ciftci@hazelcast.com>
2023-07-04 23:06:39 +0300

    Added e2e tests for map and cache for validating if native memory enabled on HZ

--- test/e2e/hazelcast_cache_test.go
+++ test/e2e/hazelcast_cache_test.go
@@ -68,4 +68,21 @@ var _ = Describe("Hazelcast Cache Config", Label("cache"), func() {
 		Expect(string(cacheConfig.InMemoryFormat)).Should(Equal(string(c.Spec.InMemoryFormat)))
 	})
 
+	When("Native Memory is not enabled for Hazelcast CR", func() {
+		It("should fail with InMemoryFormat value is set to NativeMemory", Label("fast"), func() {
+			setLabelAndCRName("hch-3")
+			hazelcast := hazelcastconfig.Default(hzLookupKey, ee, labels)
+			CreateHazelcastCR(hazelcast)
+
+			By("creating the cache config with NativeMemory")
+			c := hazelcastconfig.DefaultCache(chLookupKey, hazelcast.Name, labels)
+			c.Spec.InMemoryFormat = hazelcastcomv1alpha1.InMemoryFormatNative
+
+			Expect(k8sClient.Create(context.Background(), c)).Should(Succeed())
+			c = assertDataStructureStatus(chLookupKey, hazelcastcomv1alpha1.DataStructureFailed, &hazelcastcomv1alpha1.Cache{}).(*hazelcastcomv1alpha1.Cache)
+
+			Expect(c.Status.Message).To(ContainSubstring("Native Memory must be enabled at Hazelcast"))
+		})
+	})
+
 })
