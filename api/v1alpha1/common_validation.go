e8f2664c3c781e3d30247b8f75671827c71172cd
Dominik Zeromski <dominik.zeromski@hazelcast.com>
2023-08-30 10:56:50 +0200

    Add HotBackup validation file

--- api/v1alpha1/common_validation.go
+++ api/v1alpha1/common_validation.go
@@ -1,9 +1,7 @@
 package v1alpha1
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
 
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -12,29 +10,6 @@ import (
 
 	n "github.com/hazelcast/hazelcast-platform-operator/internal/naming"
 )
-
-func ValidateAppliedPersistence(persistenceEnabled bool, h *Hazelcast) *field.Error {
-	if !persistenceEnabled {
-		return nil
-	}
-	s, ok := h.ObjectMeta.Annotations[n.LastSuccessfulSpecAnnotation]
-	if !ok {
-		return field.InternalError(field.NewPath("spec"), fmt.Errorf("hazelcast resource %s is not successfully started yet", h.Name))
-	}
-
-	lastSpec := &HazelcastSpec{}
-	err := json.Unmarshal([]byte(s), lastSpec)
-	if err != nil {
-		return field.InternalError(field.NewPath("spec"), fmt.Errorf("error parsing last Hazelcast spec for update errors: %w", err))
-	}
-
-	if !lastSpec.Persistence.IsEnabled() {
-		return field.Invalid(field.NewPath("spec").Child("persistenceEnabled"), lastSpec.Persistence.IsEnabled(),
-			"Persistence must be enabled at Hazelcast")
-	}
-
-	return nil
-}
 
 func validateDSSpecUnchanged(obj client.Object, errorLists ...field.ErrorList) error {
 	var allErrs field.ErrorList
