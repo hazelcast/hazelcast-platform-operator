5b0de72893b75a194146fb0eea2d3d52bb1ba6d9
Dominik Zeromski <dominik.zeromski@hazelcast.com>
2023-08-30 10:52:55 +0200

    [CN-960] Refactor common validation to avoid repeating work

--- api/v1alpha1/common_validation.go
+++ api/v1alpha1/common_validation.go
@@ -13,29 +13,6 @@ import (
 	n "github.com/hazelcast/hazelcast-platform-operator/internal/naming"
 )
 
-func ValidateAppliedNativeMemory(inMemoryFormat InMemoryFormatType, h *Hazelcast) *field.Error {
-	if inMemoryFormat != InMemoryFormatNative {
-		return nil
-	}
-
-	s, ok := h.ObjectMeta.Annotations[n.LastSuccessfulSpecAnnotation]
-	if !ok {
-		return field.InternalError(field.NewPath("spec"), fmt.Errorf("hazelcast resource %s is not successfully started yet", h.Name))
-	}
-
-	lastSpec := &HazelcastSpec{}
-	err := json.Unmarshal([]byte(s), lastSpec)
-	if err != nil {
-		return field.InternalError(field.NewPath("spec"), fmt.Errorf("error parsing last Hazelcast spec for update errors: %w", err))
-	}
-
-	if !lastSpec.NativeMemory.IsEnabled() {
-		return field.Invalid(field.NewPath("spec").Child("inMemoryFormat"), lastSpec.NativeMemory.IsEnabled(),
-			"Native Memory must be enabled at Hazelcast")
-	}
-	return nil
-}
-
 func ValidateAppliedPersistence(persistenceEnabled bool, h *Hazelcast) *field.Error {
 	if !persistenceEnabled {
 		return nil
