826fd50af60ceeaf2a0bbda76744d618ec55a393
devOpsHelm <54980549+devOpsHelm@users.noreply.github.com>
2024-02-05 23:27:19 +0100

    Update Hazelcast and MC version (#928)
    Signed-off-by: devOpsHelm <devopshelm@hazelcast.com>

--- api/v1alpha1/managementcenter_types.go
+++ api/v1alpha1/managementcenter_types.go
@@ -16,7 +16,7 @@ type ManagementCenterSpec struct {
 	Repository string `json:"repository,omitempty"`
 
 	// Version of Management Center.
-	// +kubebuilder:default:="5.3.2"
+	// +kubebuilder:default:="5.3.3"
 	// +optional
 	Version string `json:"version,omitempty"`
 
