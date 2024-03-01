0579e1148b57560bc0862935732be0c4a3f69583
devOpsHelm <devopshelm@hazelcast.com>
2024-03-01 20:50:33 +0000

    Bump 'platform-operator-agent' image version to 0.1.24
    Signed-off-by: devOpsHelm <devopshelm@hazelcast.com>

--- api/v1alpha1/hazelcast_types.go
+++ api/v1alpha1/hazelcast_types.go
@@ -98,7 +98,7 @@ type HazelcastSpec struct {
 	Persistence *HazelcastPersistenceConfiguration `json:"persistence,omitempty"`
 
 	// B&R Agent configurations
-	// +kubebuilder:default:={repository: "docker.io/hazelcast/platform-operator-agent", version: "0.1.23"}
+	// +kubebuilder:default:={repository: "docker.io/hazelcast/platform-operator-agent", version: "0.1.24"}
 	Agent AgentConfiguration `json:"agent,omitempty"`
 
 	// Jet Engine configuration
@@ -618,7 +618,7 @@ type AgentConfiguration struct {
 	Repository string `json:"repository,omitempty"`
 
 	// Version of Hazelcast Platform Operator Agent.
-	// +kubebuilder:default:="0.1.23"
+	// +kubebuilder:default:="0.1.24"
 	// +optional
 	Version string `json:"version,omitempty"`
 
