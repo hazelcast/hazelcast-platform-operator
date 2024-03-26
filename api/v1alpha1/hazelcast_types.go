c3df5df51ad32d5fefe648e87ad1ebc2fa0e4345
kmetin <kutluhan.metin@hazelcast.com>
2024-03-26 16:47:46 +0300

    upgrade agent version

--- api/v1alpha1/hazelcast_types.go
+++ api/v1alpha1/hazelcast_types.go
@@ -98,7 +98,7 @@ type HazelcastSpec struct {
 	Persistence *HazelcastPersistenceConfiguration `json:"persistence,omitempty"`
 
 	// B&R Agent configurations
-	// +kubebuilder:default:={repository: "docker.io/kutluhanhazelcast/platform-operator-agent", version: "0.1.32"}
+	// +kubebuilder:default:={repository: "docker.io/kutluhanhazelcast/platform-operator-agent", version: "0.1.24"}
 	Agent AgentConfiguration `json:"agent,omitempty"`
 
 	// Jet Engine configuration
@@ -664,7 +664,7 @@ type AgentConfiguration struct {
 	Repository string `json:"repository,omitempty"`
 
 	// Version of Hazelcast Platform Operator Agent.
-	// +kubebuilder:default:="0.1.32"
+	// +kubebuilder:default:="0.1.24"
 	// +optional
 	Version string `json:"version,omitempty"`
 
