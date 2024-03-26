ecfbaf2216038b1611b57982d65d77a148cbab5c
kmetin <kutluhan.metin@hazelcast.com>
2024-03-26 17:25:26 +0300

    update agent repository to correct one

--- api/v1alpha1/hazelcast_types.go
+++ api/v1alpha1/hazelcast_types.go
@@ -98,7 +98,7 @@ type HazelcastSpec struct {
 	Persistence *HazelcastPersistenceConfiguration `json:"persistence,omitempty"`
 
 	// B&R Agent configurations
-	// +kubebuilder:default:={repository: "docker.io/kutluhanhazelcast/platform-operator-agent", version: "0.1.24"}
+	// +kubebuilder:default:={repository: "docker.io/hazelcast/platform-operator-agent", version: "0.1.24"}
 	Agent AgentConfiguration `json:"agent,omitempty"`
 
 	// Jet Engine configuration
@@ -658,8 +658,8 @@ func (c *UserCodeDeploymentConfig) IsRemoteURLsEnabled() bool {
 }
 
 type AgentConfiguration struct {
-	// Repository to pull Hazelcast Platform Operator Agent(https://github.com/kutluhanhazelcast/platform-operator-agent)
-	// +kubebuilder:default:="docker.io/kutluhanhazelcast/platform-operator-agent"
+	// Repository to pull Hazelcast Platform Operator Agent(https://github.com/hazelcast/platform-operator-agent)
+	// +kubebuilder:default:="docker.io/hazelcast/platform-operator-agent"
 	// +optional
 	Repository string `json:"repository,omitempty"`
 
