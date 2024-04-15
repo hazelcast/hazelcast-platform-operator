1769a3172ba3fbef33649d05f350191b7d913b04
aborschuk <alexander.borschuk@hazelcast.com>
2024-04-16 00:52:44 +0200

    Change HZ and MCversion to 5.4.0

--- test/e2e/config/hazelcast/config.go
+++ test/e2e/config/hazelcast/config.go
@@ -170,7 +170,7 @@ var (
 		return hazelcastcomv1alpha1.HazelcastSpec{
 			ClusterSize:          pointer.Int32(clusterSize),
 			Repository:           repo(true),
-			Version:              "5.4.0-SNAPSHOT",
+			Version:              "5.4.0",
 			LicenseKeySecretName: licenseKey(true),
 			LoggingLevel:         hazelcastcomv1alpha1.LoggingLevelDebug,
 			CPSubsystem: &hazelcastcomv1alpha1.CPSubsystem{
@@ -186,7 +186,7 @@ var (
 		return hazelcastcomv1alpha1.HazelcastSpec{
 			ClusterSize:          pointer.Int32(clusterSize),
 			Repository:           repo(true),
-			Version:              "5.4.0-SNAPSHOT",
+			Version:              "5.4.0",
 			LicenseKeySecretName: licenseKey(true),
 			LoggingLevel:         hazelcastcomv1alpha1.LoggingLevelDebug,
 			Persistence: &hazelcastcomv1alpha1.HazelcastPersistenceConfiguration{
